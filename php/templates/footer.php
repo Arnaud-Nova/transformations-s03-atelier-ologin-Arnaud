4971c85577a212ff5e2c028fdbe0313ee52e5e44
Arnaud-Nova <calmelsarnaud@gmail.com>
2018-12-14 11:20:24 +0100

    ajout templates

---
+++ php/templates/footer.php
@@ -0,0 +1,8 @@
+   
+    <footer id="footer">
+      Rejoignez-nous
+    </footer>
+  </div>
+  <script src="../js/app.js"></script>
+</body>
+</html>
---
+++ php/templates/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>oLogin</title>
+  <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700,700i" rel="stylesheet"> 
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+  
+  <div id="app">
+    <header id="header">
+      <h1 id="app-title"><a href="#">oLogin</a></h1>
+      <nav id="nav">
+        <a href="./">Accueil</a>
+        <a href="#">Profil</a>
+        <a href="#">À propos</a>
+        <a href="#">FAQ</a>
+        <a href="#">Contact</a>
+      </nav>
+    </header>
